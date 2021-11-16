bd8111645455a0b251665aaa52d2762f7ac8b399
Delimaci <gabriellec9@outlook.com>
2021-11-16 15:54:11 +0000

    Revert "Create INSERT_Employees.sql"
    This reverts commit 266626b32caac18a332a81f9fdd5f0e60b244942.

--- SQL/INSERT_Employees.sql
+++
@@ -1,4 +0,0 @@
-BEGIN TRANSACTION;
-INSERT INTO "Employee" ("Employee_ID","First_Name","Last_Name","Address","Postcode","Role","Managed_By") VALUES (1,'Mark','Jones','35 Wilson Rd, Cardiff','CF5 4LL','Manager',''),
- (2,'Charlotte','Walker','11 Ffordd Yr Afon, Cardiff','CF15 9TT','Receptionist','1');
-COMMIT;
