8b90faabd0eef9c5a2a86a53258ccc40434f41db
Delimaci <gabriellec9@outlook.com>
2021-11-18 16:22:54 +0000

    Revert "Create INSERT_GUEST_TABLE.sql"
    This reverts commit 00a17b4353c6cbb6f3fa27488e3e1fd7d68f3c9d.

--- SQL/INSERT_GUEST_TABLE.sql
+++
@@ -1,3 +0,0 @@
-BEGIN TRANSACTION;
-INSERT INTO "Guest" ("Guest_ID","Guest_Title","Guest_First_Name","Guest_Surname","Guest_Tel","Guest_Email") VALUES (1,'Mrs','Sandra','Edwards',7462911837,'SandraEd1@outlook.com');
-COMMIT;
